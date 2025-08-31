ed980d03a8e4ee1f275490d4aee9738927ab762d
Muhammad Ranju <mdranju23@gmail.com>
2025-08-31 23:06:31 +0600

    feat: added login pages and layout

---
+++ app/(auth)/login/page.tsx
@@ -0,0 +1,11 @@
+// import { LoginForm } from "@/components/login-form"
+
+import { LoginForm } from "@/components/auth/LoginForm";
+
+export default function LoginPage() {
+  return (
+    <div className="min-h-screen bg-gray-50 flex items-center justify-center p-4">
+      <LoginForm />
+    </div>
+  )
+}

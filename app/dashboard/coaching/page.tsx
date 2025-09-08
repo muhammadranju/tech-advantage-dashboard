f4ac45c590966e7f1bcc800d53a0ab6625380998
Muhammad Ranju <mdranju23@gmail.com>
2025-09-09 00:17:56 +0600

    feat: added coaching page design

--- app/dashboard/coaching/page.tsx
+++ app/dashboard/coaching/page.tsx
@@ -1,5 +1,42 @@
-const page = () => {
-  return <div>Users</div>;
+import { CoachingComponent } from "@/components/coaching/CoachingComponent";
+import { StatsCards } from "@/components/dashboard/StatsCards";
+import { UserManagement } from "@/components/users/UserManagement";
+import { FileText } from "lucide-react";
+
+const stats = [
+  {
+    title: "Total Application",
+    value: "8,642",
+    change: "+6.3%",
+    changeType: "positive" as const,
+    icon: FileText,
+  },
+  {
+    title: "Total Approve Application",
+    value: "8,642",
+    change: "+12.5%",
+    changeType: "positive" as const,
+    icon: FileText,
+  },
+  {
+    title: "Total Premium User",
+    value: "15,842",
+    change: "+32.5%",
+    changeType: "positive" as const,
+    icon: FileText,
+  },
+];
+const Coaching = () => {
+  return (
+    <div className="px-10 py-4">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+        {stats.map((stat) => (
+          <StatsCards stat={stat} key={stat.title} />
+        ))}
+      </div>
+      <CoachingComponent />
+    </div>
+  );
 };
 
-export default page;
+export default Coaching;

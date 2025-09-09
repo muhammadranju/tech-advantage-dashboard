85c38bf073a53c949dd02b8523959e2002cbef40
Muhammad Ranju <mdranju23@gmail.com>
2025-09-10 05:06:27 +0600

    feat: update dashboard overview page design

--- app/dashboard/overview/page.tsx
+++ app/dashboard/overview/page.tsx
@@ -1,18 +1,10 @@
-import { BottomCharts } from "@/components/dashboard/BottomCharts";
 import { RevenueChart } from "@/components/dashboard/RevenueChart";
 import { StatsCards } from "@/components/dashboard/StatsCards";
-import { BadgeCheckIcon, CircleDollarSign, FileText } from "lucide-react";
+import { FileText } from "lucide-react";
 import { PiUsersThreeBold } from "react-icons/pi";
 
 // stats data
 const stats = [
-  {
-    title: "Total Revenue",
-    value: "$124,563",
-    change: "+12.5%",
-    changeType: "positive" as const,
-    icon: CircleDollarSign,
-  },
   {
     title: "Total Users",
     value: "8,642",
@@ -27,19 +19,12 @@ const stats = [
     changeType: "positive" as const,
     icon: FileText,
   },
-  {
-    title: "Total Premium User",
-    value: "15,842",
-    change: "-2.1%",
-    changeType: "negative" as const,
-    icon: BadgeCheckIcon,
-  },
 ];
 export default function Page() {
   return (
     <div className="lg:px-10 py-4 space-y-10">
       {/* Stats Cards */}
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-6">
         {stats.map((stat) => (
           <StatsCards stat={stat} key={stat.title} />
         ))}
@@ -47,7 +32,7 @@ export default function Page() {
       {/* Revenue Chart */}
       <RevenueChart />
       {/* Bottom Charts */}
-      <BottomCharts />
+      {/* <BottomCharts /> */}
     </div>
   );
 }

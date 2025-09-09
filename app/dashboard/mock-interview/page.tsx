6f73abbeb1e052ee4f693e10b39aff82f542b24c
Muhammad Ranju <mdranju23@gmail.com>
2025-09-10 05:06:03 +0600

    feat: update mock interview page design

--- app/dashboard/mock-interview/page.tsx
+++ app/dashboard/mock-interview/page.tsx
@@ -1,19 +1,11 @@
 import { StatsCards } from "@/components/dashboard/StatsCards";
-import { QuizCreator } from "@/components/discoverStrength/QuizCreator";
 import { MockInterviewQuiz } from "@/components/mockInterview/MockInterviewQuiz";
 import { FileText } from "lucide-react";
 const stats = [
   {
     title: "Total Participant",
-    value: "$124,563",
+    value: "124,563",
     change: "+12.5%",
-    changeType: "positive" as const,
-    icon: FileText,
-  },
-  {
-    title: "Top Achiever",
-    value: "8,642",
-    change: "+6.3%",
     changeType: "positive" as const,
     icon: FileText,
   },
@@ -22,7 +14,7 @@ const stats = [
 const MockInterview = () => {
   return (
     <div className="px-10 py-4">
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-6">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
         {stats.map((stat) => (
           <StatsCards stat={stat} key={stat.title} />
         ))}

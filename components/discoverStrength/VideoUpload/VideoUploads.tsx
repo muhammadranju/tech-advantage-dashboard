d087a4416724b6d2eee1f408e3da65af779ce8f8
Muhammad Ranju <mdranju23@gmail.com>
2025-09-15 22:32:26 +0600

    feat: added show videos and update video upload component

--- components/discoverStrength/VideoUpload/VideoUploads.tsx
+++ components/discoverStrength/VideoUpload/VideoUploads.tsx
@@ -1,6 +1,4 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 "use client";
-import { Badge } from "@/components/ui/badge";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import {
@@ -10,73 +8,10 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
-import { Play } from "lucide-react";
 
-interface VideoRange {
-  label: string;
-  videos: number;
-  videosData: any;
-}
-
-interface VideoUploadsProps {
-  videoRanges: VideoRange[];
-  title: string;
-}
-
-export const VideoUploads = ({ videoRanges, title }: VideoUploadsProps) => {
-  console.log(videoRanges);
+export const VideoUploads = () => {
   return (
     <div className="mx-auto mt-10 space-y-8 bg-white ">
-      {/* Header */}
-      <h1 className="text-3xl font-bold text-gray-900">{title}&apos;s Video</h1>
-
-      {/* Video Ranges */}
-      <div className="space-y-6">
-        {videoRanges.map((range, rangeIndex) => (
-          <div key={rangeIndex} className="space-y-3">
-            <Badge
-              variant="secondary"
-              className="bg-white shadow border text-sm text-foreground hover:bg-muted"
-            >
-              {range.label}
-            </Badge>
-
-            <div className="grid grid-cols-3 gap-4">
-              {Array.from({ length: range.videos }).map((_, videoIndex) => {
-                return (
-                  <div key={videoIndex}>
-                    <div className="relative" key={videoIndex}>
-                      <div>
-                        <img
-                          src={range.videosData[videoIndex].thumbnail}
-                          alt={range.videosData[videoIndex].title}
-                          className="w-full h-64 object-cover rounded-xl"
-                        />
-                        <p className="font-bold text-xl">
-                          {range.videosData[videoIndex].title}
-                        </p>
-                      </div>
-                      <a
-                        href={range.videosData[videoIndex].url}
-                        target="_blank"
-                        rel="noreferrer"
-                      >
-                        <Button
-                          variant="outline"
-                          className="gap-2  absolute top-30 left-55  p-"
-                        >
-                          <Play className="h-8 w-8 text-2xl" />
-                        </Button>
-                      </a>
-                    </div>
-                  </div>
-                );
-              })}
-            </div>
-          </div>
-        ))}
-      </div>
-
       {/* Form Controls */}
       <div className="space-y-4 pt-4">
         <div className="space-y-2">

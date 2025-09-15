8b4fc57238e44163f82b23044d3a250eac5f915e
Muhammad Ranju <mdranju23@gmail.com>
2025-09-15 22:47:26 +0600

    feat: update app sidebar, add same

--- components/sidebar/AppSidebar.tsx
+++ components/sidebar/AppSidebar.tsx
@@ -1,6 +1,5 @@
 "use client";
-import { CgLogOut } from "react-icons/cg";
-import type * as React from "react";
+import { Button } from "@/components/ui/button";
 import {
   Sidebar,
   SidebarGroup,
@@ -12,10 +11,14 @@ import {
   SidebarRail,
   useSidebar,
 } from "@/components/ui/sidebar";
-import { CircleDollarSign } from "lucide-react";
+import { ChartGantt, CircleDollarSign } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import type * as React from "react";
+import { CgLogOut } from "react-icons/cg";
+import { FiSidebar } from "react-icons/fi";
+import { IoMdBusiness } from "react-icons/io";
 import { LuSettings } from "react-icons/lu";
 import {
   PiGraduationCapBold,
@@ -24,9 +27,6 @@ import {
   PiTentBold,
   PiUsersThreeBold,
 } from "react-icons/pi";
-import { Button } from "@/components/ui/button";
-import { FiSidebar } from "react-icons/fi";
-import { IoMdBusiness } from "react-icons/io";
 
 // This is sample data.
 const data = {
@@ -49,8 +49,13 @@ const data = {
           icon: <IoMdBusiness />,
         },
         {
-          title: "Discover Strength",
-          url: "/dashboard/discover-strength",
+          title: "Business Planning",
+          url: "/dashboard/business-planning",
+          icon: <ChartGantt />,
+        },
+        {
+          title: "Success Path",
+          url: "/dashboard/success-path",
           icon: <PiPersonSimpleRunBold />,
         },
         {
@@ -90,7 +95,7 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
       >
         <FiSidebar className="text-black w-10 hover:text-gray-600 transition-colors  rounded-full" />
       </div>
-      <SidebarHeader className="text-white relative">
+      <SidebarHeader className="text-white relative ">
         <div className="flex items-center ">
           <Image src="/logo.png" alt="logo" width={45} height={61} />
           <h1 className="font-bold text-lg text-white">TECH ADVANTAGE</h1>
@@ -109,7 +114,7 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
                     asChild
                     isActive={pathname === item.url}
                   >
-                    <Link className="p-6" href={item.url}>
+                    <Link className="p-6 my-2" href={item.url}>
                       <span className="mr-2 text-lg"> {item.icon}</span>{" "}
                       {item.title}
                     </Link>
@@ -121,7 +126,9 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
         </SidebarGroup>
       ))}
 
-      <div className="flex justify-center items-center gap-4 mb-4 px-4 ">
+      <div className="flex flex-col justify-center items-center gap-4 mb-4  px-4 ">
+        <hr className="h-px px-10 my-8  bg-neutral-00 border-0" />
+
         <Button className="bg-neutral-600 hover:bg-neutral-700 py-6 w-full text-lg text-white">
           <CgLogOut className="rotate-180 h-24 w-24" />{" "}
           {/* Increased icon size to h-24 w-24 */}

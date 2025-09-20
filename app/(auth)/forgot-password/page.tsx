90ade2f5718b0e05ad784af011a80d76d970ea0c
Muhammad Ranju <mdranju23@gmail.com>
2025-09-21 01:01:26 +0600

    docs: update auth page titles

--- app/(auth)/forgot-password/page.tsx
+++ app/(auth)/forgot-password/page.tsx
@@ -4,6 +4,7 @@ import React from "react";
 const page = () => {
   return (
     <div>
+      <title>TechAdvantage - Forgot Password</title>
       <ForgotPassword />
     </div>
   );
--- app/(auth)/reset-password/page.tsx
+++ app/(auth)/reset-password/page.tsx
@@ -4,6 +4,7 @@ import ResetPassword from "../forgot-password/page";
 const page = () => {
   return (
     <div>
+      <title>TechAdvantage - Reset Password</title>
       <ResetPassword />
     </div>
   );

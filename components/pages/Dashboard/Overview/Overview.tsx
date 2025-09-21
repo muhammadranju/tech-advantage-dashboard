4eba136f62989baaeb722da1f3ab78ee39e08e98
Muhammad Ranju <mdranju23@gmail.com>
2025-09-22 04:10:05 +0600

    feat: update dashboard chart design and adjust card alignment

--- components/pages/Dashboard/Overview/Overview.tsx
+++ components/pages/Dashboard/Overview/Overview.tsx
@@ -24,7 +24,7 @@ export default function OverviewPage() {
   return (
     <div className="lg:px-10 py-4 space-y-10">
       {/* Stats Cards */}
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-6">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
         {stats.map((stat) => (
           <StatsCards stat={stat} key={stat.title} />
         ))}

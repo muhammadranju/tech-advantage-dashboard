c48c24cb4d323a3157fc14516d3382af9e126162
Muhammad Ranju <mdranju23@gmail.com>
2025-09-28 05:10:21 +0600

    feat: update dashboard design

---
+++ app/dashboard/course/[moduleID]/page.tsx
@@ -0,0 +1,11 @@
+import CourseModulesPage from "@/components/pages/Dashboard/Course/CourseModulesPage";
+
+const Module = () => {
+  return (
+    <div>
+      <CourseModulesPage />
+    </div>
+  );
+};
+
+export default Module;
---
+++ app/dashboard/course/page.tsx
@@ -0,0 +1,12 @@
+import CoursePage from "@/components/pages/Dashboard/Course/CoursePage";
+
+const page = () => {
+  return (
+    <div>
+      <title>TechAdvantage - Course</title>
+      <CoursePage />
+    </div>
+  );
+};
+
+export default page;

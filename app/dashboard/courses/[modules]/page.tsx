37bebfacb1c2bfb968e18ff716f5644a03dd31e9
Muhammad Ranju <mdranju23@gmail.com>
2025-09-29 03:50:46 +0600

    feat: refactor all pages code and content

---
+++ app/dashboard/courses/[modules]/page.tsx
@@ -0,0 +1,12 @@
+import ModulesPage from "@/components/pages/Dashboard/Course/ModulesPage";
+
+const page = () => {
+  return (
+    <div>
+      <title>TechAdvantage - Course</title>
+      <ModulesPage />
+    </div>
+  );
+};
+
+export default page;

d0a51f6997d6d9455539f3d132a200fd4c8b4ad5
Muhammad Ranju <mdranju23@gmail.com>
2025-10-09 03:06:11 +0600

    feat(dashboard): add feedback page component
    Introduce new feedback page in dashboard section using FeedbackPage component

---
+++ app/dashboard/feedback/page.tsx
@@ -0,0 +1,12 @@
+import FeedbackPage from "@/components/pages/Dashboard/Feedback/FeedbackPage";
+
+const page = () => {
+  return (
+    <div>
+      <title>Feedback - TechAdvantage</title>
+      <FeedbackPage />
+    </div>
+  );
+};
+
+export default page;

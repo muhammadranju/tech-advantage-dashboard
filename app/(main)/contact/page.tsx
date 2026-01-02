8724466f96529c86f3db8ea111f74ff8b775e2aa
Muhammad Ranju <mdranju23@gmail.com>
2026-01-03 03:06:38 +0600

    feat: Implement initial landing page with global styles, assets, and placeholder legal/info pages.

---
+++ app/(main)/contact/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function page() {
+  return (
+    <div className="flex items-center justify-center h-screen text-4xl font-bold">
+      Coming Soon
+    </div>
+  );
+}
+
+export default page;

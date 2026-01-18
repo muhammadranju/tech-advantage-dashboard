645d9ebe1048df58ab5a980a11fad13c9151da6f
Muhammad Ranju <mdranju23@gmail.com>
2026-01-18 06:33:38 +0600

    feat: implement a comprehensive landing page with navigation and feature sections, and configure the root page to redirect to the dashboard.

--- app/(main)/index/page.tsx
+++ app/(main)/index/page.tsx
@@ -12,7 +12,7 @@ import {
 import Link from "next/link";
 import { useEffect } from "react";
 
-export default function Page() {
+export default function HomePage() {
   useEffect(() => {
     // Scroll to top on page load
     window.scrollTo({
--- app/page.tsx
+++ app/page.tsx
@@ -2,3 +2,16 @@ import { redirect, RedirectType } from "next/navigation";
 export default function Page() {
   redirect("/dashboard", RedirectType.push);
 }
+
+// import React from "react";
+// import HomePage from "./(main)/index/page";
+
+// function page() {
+//   return (
+//     <>
+//       <HomePage />
+//     </>
+//   );
+// }
+
+// export default page;
